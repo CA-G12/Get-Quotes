b3d5cc1f708f7556746b12c8fccd8c08f4b96e7d
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-04 10:16:40 +0300

    Changed the functions to fuction expression and fix some bugs close #33

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,6 @@
 // ? Getting HTML elements.
 const dropDownMenu = document.getElementById("type");
 const searchBtn = document.getElementById("search-btn");
-const searchInput = document.getElementById("search");
 // ? Create handle dom function.
 const handleDOM = (data, quoteKey, authorKey, quotesSection) => {
   // ? Getting the section of all quotes.
@@ -27,10 +26,10 @@ const handleDOM = (data, quoteKey, authorKey, quotesSection) => {
 };
 
 // ? Creating the function which is responsible for fetching the anime API with random endpoint.
-function animeFetchAll() {
+const animeFetchAll = () => {
   const url = "https://animechan.vercel.app/api/quotes";
 
-  fetch(url, function (data) {
+  fetch(url, (data) => {
     handleDOM(
       truncate(data, 10),
       "quote",
@@ -38,13 +37,13 @@ function animeFetchAll() {
       ".container__section-two"
     );
   });
-}
+};
 
 // ? Creating the function which is responsible for fetching the go quotes API with random endpoint.
-function byTagFetchAll() {
+const byTagFetchAll = () => {
   const url = "https://goquotes-api.herokuapp.com/api/v1/random?count=10";
 
-  fetch(url, function (data) {
+  fetch(url, (data) => {
     handleDOM(
       truncate(data.quotes, 10),
       "text",
@@ -52,13 +51,13 @@ function byTagFetchAll() {
       ".container__section-two"
     );
   });
-}
+};
 
 // ? Calling the function as long as the tag value is the default.
 byTagFetchAll();
 
 // ? Adding event listener to listen to select element value change and call the functions depending on the value.
-dropDownMenu.addEventListener("change", function () {
+dropDownMenu.addEventListener("change", () => {
   if (dropDownMenu.value === "anime") {
     animeFetchAll();
   } else {
@@ -67,8 +66,9 @@ dropDownMenu.addEventListener("change", function () {
 });
 
 searchBtn.addEventListener("click", () => {
-  if (dropDownMenu.value === "tags") {
-    if (searchInput != "")
+  const searchInput = document.getElementById("search");
+  if (searchInput != "") {
+    if (dropDownMenu.value === "tags") {
       fetch(
         `https://goquotes-api.herokuapp.com/api/v1/all?type=tag&val=${searchInput.value}`,
         (data) => {
@@ -80,17 +80,18 @@ searchBtn.addEventListener("click", () => {
           );
         }
       );
-  } else {
-    fetch(
-      `https://animechan.vercel.app/api/quotes/character?name=${searchInput.value}`,
-      (data) => {
-        handleDOM(
-          truncate(data),
-          "quote",
-          "character",
-          ".container__section-two"
-        );
-      }
-    );
+    } else {
+      fetch(
+        `https://animechan.vercel.app/api/quotes/character?name=${searchInput.value}`,
+        (data) => {
+          handleDOM(
+            truncate(data),
+            "quote",
+            "character",
+            ".container__section-two"
+          );
+        }
+      );
+    }
   }
 });
